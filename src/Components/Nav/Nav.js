178040b0a1965ca159dee95a227b9b3ed634a881
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2018-12-28 18:44:04 -0200

    making menu in nav component

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import st from './style.js';
-import menu from '../../Assets/Images/menu.svg';
-import logo from '../../Assets/Images/calculator.svg'
+import menuIcon from '../../Assets/Images/menu.svg';
+import logo from '../../Assets/Images/calculator.svg';
+import close from '../../Assets/Images/left-arrow.svg';
 
 export default class Nav extends Component {
+    state = {
+        menuOpen: false,
+    }
+    handleMenu = (e) => {
+        let menuState = this.state.menuOpen;
+        console.log(menuState);
+        this.setState({
+            menuOpen: !menuState,
+        });
+    }
     render() {
+        const menu = (
+            <div style={st.menuContainer} onClick={this.handleMenu}>
+                <div style={st.content}>
+                    <div style={st.logoContainer}>
+                        <img src={logo} style={st.imgLogo} alt=" logo do webapp allboutfood " />
+                    </div>
+                    <div>
+                        <img src={close} style={st.closeIcon} onClick={this.handleMenu} alt=" fechar menu " />
+                    </div>
+                </div>
+            </div >
+        );
         return (
-            <nav style={st.container}>
+            <div>
+                <nav style={st.container}>
+                    <div onClick={this.handleMenu}>
+                        <img src={menuIcon} style={st.menu} alt="icone de menu" />
+                    </div>
+                    <div style={st.logo}>
+                        <img src={logo} style={st.imgLogo} alt="logo do webapp allboutfood" />
+                    </div>
+                </nav>
                 <div>
-                    <img src={menu} style={st.menu} alt="icone de menu" />
+                    {
+                        this.state.menuOpen ? menu : null
+                    }
                 </div>
-                <div style={st.logo}>
-                    <img src={logo} style={st.imgLogo} alt="logo do webapp allboutfood" />
-                </div>
-            </nav>
+            </div>
         );
     }
 }
--- src/Components/Nav/style.js
+++ src/Components/Nav/style.js
@@ -15,10 +15,39 @@ export default {
     menu: {
         height: "25px",
         display: "flex",
+        cursor: "pointer",
     },
     imgLogo: {
         height: "45px",
         display: "flex",
         padding: "10px 0px",
+    },
+    menuContainer: {
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "rgba(0,0,0,0.4)",
+        position: "absolute",
+        top: "0px",
+    },
+    content: {
+        minHeight: "100vh",
+        width: "30%",
+        disply: "flex",
+        float: "left",
+        backgroundColor: "white",
+        position: "relative",
+        minWidth: "250px",
+    },
+    logoContainer: {
+        display: "flex",
+        justifyContent: "center",
+        height: "45px",
+    },
+    closeIcon: {
+        position: "absolute",
+        height: "35px",
+        top: "0px",
+        right: "10px",
+        cursor: "pointer",
     },
 }

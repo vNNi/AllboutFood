37deb607ace4bb35851d2e4a44a149a0519dab42
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-01-06 17:16:19 -0200

    adding route to page

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import Index from './Pages/Index/Index';
 import Result from './Pages/Result/index';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import ServerError from './Pages/500/index';
 ReactDOM.render(
     <Router>
         <Switch>
             <Route exact={true} path="/" component={Index} />
             <Route path="/landing" component={Index} />
             <Route path="/result" component={Result} />
+            <Route path="/500" component={ServerError} />
         </Switch>
     </Router>
 

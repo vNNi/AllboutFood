37f17da5b1c580e35803d489f1d0836d926978ff
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-01-06 01:18:29 -0200

    adding some colors

--- src/Assets/Colors/index.js
+++ src/Assets/Colors/index.js
@@ -1,4 +1,8 @@
 export default {
     primaryBlue: "rgb(127, 165, 226)",
-    primaryYellow: "rgb(224, 224, 235)",
+    primaryYellow: "rgb(255, 255, 102)",
+    lightGreen: "#2ecc71",
+    lightGrey: "#e6e6e6",
+    lightRed: "#e74c3c",
+    lightYellow: "#f1c40f",
 }

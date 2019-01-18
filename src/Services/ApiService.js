86a09068b1c97cf1a3d5928724f1215ad5beedb2
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-01-18 16:17:27 -0200

    new handle apis services

--- src/Services/ApiService.js
+++ src/Services/ApiService.js
@@ -1,6 +1,9 @@
-export default class ApiService {
+import { newRecipe } from '../Actions/actionCreators';
+import store from '../Store/store';
+import { push } from 'connected-react-router'
+export default (class ApiService {
     constructor() {
-        this.url = `https://api.edamam.com/api/nutrition-details?app_id=33c387e3&app_key=8f59ea5bfa153e2a2099064a3465ba8a`;
+        this.url = `https://bdac9743-f229-412e-a2b7-d607d94d86c5.mock.pstmn.io/api/nutrition-details?app_id=33c387e3&app_key=8f59ea5bfa153e2a2099064a3465ba8a`;
     }
     newRecipe(data) {
         console.log(data);
@@ -11,13 +14,21 @@ export default class ApiService {
             },
             body: JSON.stringify(data),
         };
-        return new Promise((resolve, reject) => {
-            fetch(this.url, request)
-                .then((response) => {
-                    resolve(response);
-                }).catch((error) => {
-                    reject(error);
-                });
-        });
+        fetch(this.url, request)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else if (response.status === 500) {
+                    throw new Error("server error 500")
+                }
+            }).then((result) => {
+                store.dispatch(newRecipe(result));
+            }).then(() => {
+                store.dispatch(push("/result"));
+                console.log(store.getState().postRecipe);
+            })
+            .catch((error) => {
+                // console.log(error);
+            });
     }
-}
+})

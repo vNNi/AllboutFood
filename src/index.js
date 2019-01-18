b85505b784f98b3acb308684cdc122afb02e5f61
Vinicius Komninakis <Vinicius_k2013@hotmail.com>
2019-01-18 16:17:58 -0200

    new index app with provider and connect router redux

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ import './index.css';
 import Index from './Pages/Index/Index';
 import Result from './Pages/Result/index';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import apiService from './Services/ApiService';
+import { Route, Switch } from 'react-router-dom';
 import ServerError from './Pages/500/index';
+import { Provider } from 'react-redux';
+import store, { history } from './Store/store';
+import { ConnectedRouter } from 'connected-react-router';
 ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route exact={true} path="/" component={Index} />
-            <Route path="/landing" component={Index} />
-            <Route path="/result" component={Result} />
-            <Route path="/500" component={ServerError} />
-        </Switch>
-    </Router>
-
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route exact={true} path="/" component={Index} />
+                <Route path="/landing" component={Index} />
+                <Route path="/result" component={Result} />
+                <Route path="/500" component={ServerError} />
+                <Route component={apiService} />
+            </Switch>
+        </ConnectedRouter>
+    </Provider>
     , document.getElementById('root'));
 serviceWorker.unregister();
